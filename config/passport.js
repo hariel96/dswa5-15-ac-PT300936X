a78a595c9d65837c79ae50a896f79b11d9d7709b
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2022-01-03 20:26:02 -0300

    Corrigir URL de callback.

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,8 @@ module.exports = function() {
     passport.use(new GitHubStrategy({
         clientID: 'de4c2e6477ed3b643981',
         clientSecret: 'bc02463fa3cd79854d11c2519e91e6b0c3db6f62',
-        callbackURL: 'http://localhost:5000/auth/github/callback'
+        //callbackURL: 'http://localhost:5000/auth/github/callback'
+        callbackURL: 'https://dswa5-10-ac-pt23820x.herokuapp.com/auth/github/callback'
     }, function(accessToken, refreshToken, profile, done) {
 
         Usuario.findOrCreate({ "login": profile.username }, { "nome": profile.username },
